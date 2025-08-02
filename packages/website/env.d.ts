fe11524755fd8fc44a82917c715ef1e976832fdf
the3rdchild <naufalkholisarrahman@gmail.com>
2025-08-02 15:39:53 +0000

    Move to firestore db

---
+++ packages/website/env.d.ts
@@ -0,0 +1,18 @@
+// packages/website/src/env.d.ts
+/// <reference types="vite/client" />
+
+// kalau tidak pakai Vite types, definisikan manual:
+interface ImportMetaEnv {
+    readonly VITE_FIREBASE_API_KEY: string;
+    readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+    readonly VITE_FIREBASE_PROJECT_ID: string;
+    readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+    readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly VITE_FIREBASE_APP_ID: string;
+    // plus env lain kalau ada
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
